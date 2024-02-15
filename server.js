6a8f64a68314ccc38f6f89bc64c26fee7b5197c1
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2024-02-15 16:14:41 -0500

    post method

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('server is listening....');
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.post('/', (req, res) => {
+    console.log(req);
+    res.json({ message: 'Hello World 11' });
+});
+
+app.listen(5100, () => {
+    console.log('server running....');
+});
