f84964fc68fa33145a51b0ee22c9b656bd0850d1
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2024-02-16 17:02:04 -0500

    get request

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
 
 const app = express();
 
@@ -12,7 +13,13 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-// Handlers
+let jobs = [
+    { id: nanoid(), company: 'apple', position: 'frontend developer' },
+    { id: nanoid(), company: 'google', position: 'backend developer' },
+    { id: nanoid(), company: 'meta', position: 'fullstack developer' },
+];
+
+// Routes and Handlers
 
 app.get('/', (req, res) => {
     res.send('Hello World11');
@@ -21,6 +28,10 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     // console.log(req);
     res.json({ message: 'Hello World Post method is working' });
+});
+
+app.get('/api/v1/jobs', (req, res) => {
+    res.status(200).json({ jobs });
 });
 
 const port = process.env.PORT || 5100;
