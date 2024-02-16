49416ab09488c8e1135d23acf13b8fe73281ed17
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2024-02-16 13:00:59 -0500

    morgan is working

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import morgan from 'morgan';
 
 const app = express();
+
+// Middlewares
 app.use(express.json());
-app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Handlers
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-app.post('/', (req, res) => {
-    console.log(req);
-    res.json({ message: 'Hello World 11' });
+    res.send('Hello World11');
 });
 
-app.listen(5100, () => {
-    console.log('server running....');
+app.post('/', (req, res) => {
+    // console.log(req);
+    res.json({ message: 'Hello World Post method is working' });
+});
+
+const port = process.env.PORT || 5100;
+app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`);
 });
